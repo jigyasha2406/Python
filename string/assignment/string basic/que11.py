09fe5944a4724ab5fa73c603f848edee470ef57b
jigyasha2406 <jigyasha2406@gmail.com>
2026-01-20 09:12:15 +0530

    Intial commit

--- string/assignment/string basic/que11.py
+++ string/assignment/string basic/que11.py
@@ -1,1 +1,6 @@
 #Take length and width and print Area is <area>.
+len=int(input("Enter the length"))
+wid=int(input("enter the width"))
+area=len*wid
+print(f"Area is: {area}")
+print("Area is:{}".format(area))
